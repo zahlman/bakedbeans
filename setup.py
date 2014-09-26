7db51174419bb364f4ec6a99e553f09c75e2e78d
Karl Knechtel <zahlman@gmail.com>
2014-09-25 20:10:48 -0400

    Initial work based on pypa/sampleproject. Reorganized setup.py; removed some unnecessary files.

---
+++ setup.py
@@ -0,0 +1,120 @@
+from setuptools import setup
+from os import path
+import setup_config as config
+
+
+development_status_names = [
+	'1 - Planning',
+	'2 - Pre-Alpha',
+	'3 - Alpha',
+	'4 - Beta',
+	'5 - Production/Stable',
+	'6 - Mature',
+	'7 - Inactive'
+]
+
+
+def make_classifiers_gen(breadcrumb, *options):
+	prefix = ' :: '.join(breadcrumb)
+	for option in options:
+		if option is None:
+			yield prefix
+		# Since strings are iterable, they need to be handled separately.
+		elif isinstance(option, str):
+			yield prefix + ' :: ' + option
+		else:
+			try:
+				i = iter(option)
+				n = next(i)
+			except TypeError: # Not a sequence.
+				yield '{} :: {}'.format(prefix, option)
+			except StopIteration:
+				raise ValueError("Empty sequences not permitted")
+			else:
+				yield from make_classifiers_gen(breadcrumb + (n,), *i)
+
+
+def make_classifiers(breadcrumb, *options):
+	return list(make_classifiers_gen(breadcrumb, *options))
+
+
+def make_version(*args, **kwargs):
+	result = '.'.join(str(a) for a in args)
+	if 'epoch' in kwargs:
+		result = '{}!{}'.format(kwargs['epoch'], result)
+	def single_key(normalize, *names):
+		canonical = names[0]
+		found = False
+		for name in names:
+			if name in kwargs:
+				if found:
+					raise ValueError("please specify only one of {}".format(names))
+				found = True
+				if normalize and name != canonical:
+					kwargs[canonical] = kwargs[name]
+					del kwargs[name]
+	single_key(True, 'c', 'rc', 'preview', 'pre')
+	single_key(True, 'b', 'beta')
+	single_key(True, 'a', 'alpha')
+	single_key(False, 'a', 'b', 'c')
+	single_key(True, 'post', 'r', 'rev')
+	for segment in ('a', 'b', 'c', 'post', 'dev'):
+		if segment in kwargs:
+			result = '{}.{}{}'.format(result, segment, kwargs[segment])
+	return result
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+	long_description = f.read()
+
+options = {
+	'name': config.name,
+	'version': make_version(*config.version, **config.version_qualifiers),
+	'description': config.description,
+	'long_description': long_description,
+	'url': config.url,
+	'author': config.author,
+	'author_email': config.author_email,
+	'license': config.license,
+	'classifiers': make_classifiers(
+		('Development Status', development_status_names[config.development_status - 1])
+	) + make_classifiers(
+		(), *config.additional_classifiers
+	) + make_classifiers(
+		('License',), config.license_long
+	) + make_classifiers(
+		('Programming Language', 'Python'), *config.supported_versions
+	),
+	'keywords': config.keywords,
+	'packages': [config.name],
+	'install_requires': config.dependencies
+}
+
+options['package_data'] = {
+	config.name: config.package_data + [
+		'../DESCRIPTION.rst', '../setup_config.py'
+	]
+}
+
+# TODO: look for a 'data' directory and set up data_files if appropriate
+# Although 'package_data' is the preferred approach, in some case you may
+# need to place data files outside of your packages.
+# see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+# In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+# data_files=[('my_data', ['data/data_file'])],
+
+entry_points = {}
+for script_type in ('console_scripts', 'gui_scripts'):
+	entry_points[script_type] = [
+		'{}={}'.format(k, ':'.join(v.rpartition('.')[::2]))
+		for k, v in getattr(config, script_type, {}).items()
+	]
+
+if entry_points:
+	options['entry_points'] = entry_points
+
+# Finally ready.
+setup(**options)
---
+++ setup_config.py
@@ -0,0 +1,75 @@
+# Support, if possible, for a more informative version specification.
+try:
+	from enum import IntEnum
+	status = IntEnum(
+		'status',
+		'PLANNING PREALPHA ALPHA BETA PRODUCTION MATURE INACTIVE'
+	)
+except ImportError:
+	pass
+
+
+# The project name. Also assumed to be the name of the single package that the
+# project distribution consists of.
+name = 'sample'
+
+# The version number.
+version = (1, 2, 1)
+# Qualifications for the version number - epoch, pre-release version,
+# post-release version and development version number, if applicable.
+# Allowable keys: 'epoch', 'dev';
+# at most one of 'post', 'rev', 'r';
+# and at most one of 'a', 'b', 'c', 'alpha', 'beta', 'rc', 'pre', 'preview'.
+# See PEP 440 for more information.
+version_qualifiers = {}
+
+# Short-form description of the project. A long-form description should be
+# written in DESCRIPTION.rst, in ReStructured Text format with UTF-8 encoding.
+description = 'A sample Python project'
+
+# Author details. Adjust as appropriate.
+author = 'Karl Knechtel'
+author_email = 'zahlman@gmail.com'
+author_github = 'zahlman'
+
+# The project's main homepage.
+# The default setting assumes you will host the project on Github.
+url = 'https://github.com/{}/{}'.format(author_github, name)
+
+# Licensing.
+license = 'MIT'
+# Items to use in the 'license' classifier. Please make sure this matches
+# the short license name.
+license_long = ('OSI Approved', 'MIT License')
+
+# How mature is this project?
+development_status = status.PLANNING
+
+# What versions of Python are supported.
+supported_versions = ['3.2', '3.3', '3.4']
+
+# What does your project relate to?
+keywords = 'sample setuptools development'
+
+# Any other classifiers to add, beyond the ones automatically generated.
+# See https://pypi.python.org/pypi?%3Aaction=list_classifiers for classifiers.
+# For each classifier, specify a sequence of items to join with ' :: ';
+# or use nested sequences to group multiple classifiers (see the documentation
+# for setup.make_classifiers).
+additional_classifiers = (
+	('Intended Audience', 'Developers'),
+  ('Topic', ('Software Development', 'Build Tools'))
+)
+
+# List run-time dependencies here.  These will be installed by pip when the
+# project is installed.
+dependencies = []
+
+# XXX
+package_data = []
+
+# Console scripts. Mapping from script name to fully qualified function name.
+console_scripts = {'sample': 'sample.main'}
+
+# GUI scripts. Mapping from script name to fully qualified function name.
+gui_scripts = {}
